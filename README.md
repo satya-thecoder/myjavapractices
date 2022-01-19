6418e6a11c05f4107b257dc5a27b2eea9301780c
satya-thecoder <satya020rinku@gmail.com>
2022-01-19 18:13:46 +0530

    My First Commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # myjavapractices
+
+  This is my First Repository .Here I will update my all java learnings.
+  
+  
---
+++ Test.java
@@ -0,0 +1,8 @@
+import java.lang.*;
+class Test
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Welcome to Java");
+  }
+}
